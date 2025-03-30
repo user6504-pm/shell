fe407c6c2ac1fc3667ba69b313672f8e7d5b4f03
user7845-pm <antoine.groussard00@gmail.com>
2025-03-30 21:49:12 +0200

    Update NewCCommand.cs

--- ParisShell/Commands/NewCCommand.cs
+++ ParisShell/Commands/NewCCommand.cs
@@ -3,38 +3,37 @@ using Spectre.Console;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ParisShell.Commands
 {
-    internal class NewCommand : ICommand
+    internal class NewCCommand : ICommand
     {
         public string Name => "newc";
         private readonly SqlService _sqlService;
         private readonly Session _session;
 
-        public NewCommand(SqlService sqlService, Session session) {
+        public NewCCommand(SqlService sqlService, Session session)
+        {
             _sqlService = sqlService;
             _session = session;
         }
 
         public void Execute(string[] args)
         {
-            if (!_session.IsAuthenticated)
+            AnsiConsole.Clear();
+
+            if (!_sqlService.IsConnected)
             {
-                Shell.PrintError("You must be logged to order");
+                Shell.PrintError("Vous devez être connecté à la base de données.");
                 return;
             }
 
-            if (!_session.IsInRole("CLIENT"))
+            if (!_session.IsAuthenticated || !_session.IsInRole("CLIENT"))
             {
-                AnsiConsole.MarkupLine("You must be a client to order");
+                Shell.PrintError("❌ Seuls les clients peuvent passer une commande.");
                 return;
             }
-            AnsiConsole.Clear();
-            AnsiConsole.MarkupLine("[green]Bienvenue dans l'espace de commande ![/]");
-            AnsiConsole.MarkupLine("[blue]Voici la liste des commandes disponibles :[/]");
         }
     }
 }
