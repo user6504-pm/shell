7083aa5a4b494f4962e9cd1130e8bc4e7d983bee
user7845-pm <antoine.groussard00@gmail.com>
2025-03-31 11:11:33 +0200

    Update AddQCommand.cs

--- ParisShell/Commands/AddQCommand.cs
+++ ParisShell/Commands/AddQCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace ParisShell.Commands
 {
-    internal class AddQCommand
+    internal class AddQCommand : ICommand
     {
+        public string Name => "addq";
+
+        public void Execute(string[] args)
+        {
+            AnsiConsole.Clear();
+        }
     }
 }
